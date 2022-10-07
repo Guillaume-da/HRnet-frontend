3c0be7f3ae361d67d15ce30fedd21ad3d64cd80d
Guillaume-da <singy@hotmail.fr>
2022-10-07 18:00:44 +0200

    logo removed for test

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import '../../index.css'
-import { useSelector } from 'react-redux'
+// import { useSelector } from 'react-redux'
 import { m, LazyMotion, domAnimation } from 'framer-motion'
 
 const Header = () => {
-	const darkMode = useSelector(state => state.darkMode)
+	// const darkMode = useSelector(state => state.darkMode)
 	
 	return (
-		<div className="relative h-20 sm:h-32 flex justify-between items-center bg:white dark:bg-zinc-800 mb-20  mx-2 md:mb-2 2xl:mx-20 px-4 dark:border-zinc-800 rounded dark:text-white">
-			<Link to="/" className="dark:hidden"><img src='https://ik.imagekit.io/edukqfgq5/logo_Oy91PfjehE.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1664545359607' width="112" height="103" alt="" aria-label="Add employee" className="w-28 inline-block dark:hidden absolute top-2 left-2"/></Link>
-			{darkMode ? <Link to="/"><h1 className="absolute left-0">Weather Health</h1></Link> : ''}
-			{darkMode ? <Link to="/" data-mdb-ripple="true" className="dark:hidden"><h1>Weather Health</h1></Link> : <div></div>}
+		<div className="relative h-20 sm:h-32 flex justify-between items-center bg:white dark:bg-zinc-800 mb-20  mx-2 md:mb-2 2xl:mx-28 px-4 dark:border-zinc-800 rounded dark:text-white">
+			{/* <Link to="/" className="dark:hidden"><img src='https://ik.imagekit.io/edukqfgq5/logo_Oy91PfjehE.webp?ik-sdk-version=javascript-1.4.3&updatedAt=1664545359607' width="112" height="103" alt="" aria-label="Add employee" className="w-28 inline-block dark:hidden absolute top-2 left-2"/></Link> */}
+			{/* {darkMode ?  */}<Link to="/"><h1 className="absolute left-0">Weather Health</h1></Link>{/*  : ''} */}
+			{/* {darkMode ? <Link to="/" data-mdb-ripple="true" className="dark:hidden"><h1>Weather Health</h1></Link> : <div></div>} */}
 			<div className="absolute left-1/2 -translate-x-1/2 mt-28 md:-mt-6 flex flex-col h-36 items-center justify-center">
 				<LazyMotion features={domAnimation}>
 					<m.div
